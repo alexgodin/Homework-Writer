c1b272fbc0dccb7932ed1af98a2e86a8d9f66bd1
Alex Godin <alex@alexgodin.com>
2012-01-14 23:38:10 -0500

    refactor. remove unnecessary logic.

--- homeworkwriter.py
+++ homeworkwriter.py
@@ -15,36 +15,8 @@ def choice(): #pick the period
 def write(): #write the homework
     per=input("What period is it")
     hw=input("What is the homework")
-    if per=="1":
-        agenda.write("Period 1:")
-        agenda.write(hw)
-        agenda.write("\n")
-    elif per=="2":
-        agenda.write("Period 2:")
-        agenda.write(hw)
-        agenda.write("\n")
-    elif per=="3":
-        agenda.write("Period 3:")
-        agenda.write(hw)
-        agenda.write("\n")
-    elif per=="4":
-        agenda.write("Period 4:")
-        agenda.write(hw)
-        agenda.write("\n")
-    elif per=="5":
-        agenda.write("Period 5:")
-        agenda.write(hw)
-        agenda.write("\n")
-    elif per=="6":
-        agenda.write("Period 6:")
-        agenda.write(hw)
-        agenda.write("\n")
-    elif per=="7":
-        agenda.write("Period 7:")
-        agenda.write(hw)
-        agenda.write("\n")
-    elif per=="8":
-        agenda.write("Period 8:")
+    if per.isdigit() && len(per)==1:
+        agenda.write("Period "+per+":"
         agenda.write(hw)
         agenda.write("\n")
     else:
